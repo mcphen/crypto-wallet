5e111a5baba1a0db05636794cc4ee25a9d833c69
enock <enockmambou@gmail.com>
2021-07-05 18:47:14 +0200

    integration de ant design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import React from 'react';
+
+import 'antd/dist/antd.css';
 import './App.css';
 
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>Content</Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
